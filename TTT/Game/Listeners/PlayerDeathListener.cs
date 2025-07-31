393209062c8a4a2d7b2fd13afc01f7d783891100
MSWS <imodmaker@gmail.com>
2025-07-30 22:33:36 -0700

    refactor: Refactor game listeners and enhance player state handling
    ```
    Refactor game listeners and improve player state management
    
    - Add `PlayerJoinBaseStartListener` to handle game start logic, replacing `PlayerJoinGameStartListener`.
      - Prevent game start if one is already active and ensure at least 2 players are online.
      - Notify players of game start and initialize the game with a delay.
    - Replace `PlayerJoinGameStartListener` with a streamlined implementation and delete the outdated class.
    - Introduce `PlayerDeathListener` to manage game-ending logic based on alive players and winning roles.
    - Update `IGame` interface:
      - Rename `winningTeam` to `winningRole` in the `EndGame` method.
      - Add `GetAlive` methods for player queries, including role-based filtering.
    - Add abstract `BaseListener` class to centralize dependency handling and lifecycle management for listeners.
    - Simplify `JoinStartTest` by removing unused dependencies and updating tests to reflect listener changes.
    ```

---
+++ TTT/Game/Listeners/PlayerDeathListener.cs
@@ -0,0 +1,49 @@
+using TTT.API.Events;
+using TTT.API.Role;
+using TTT.Game.Events.Player;
+using TTT.Game.Roles;
+
+namespace TTT.Game.Listeners;
+
+public class PlayerDeathListener(IServiceProvider provider)
+  : BaseListener(provider) {
+  public override string Name { get; } = nameof(PlayerDeathListener);
+
+  [EventHandler]
+  public void OnKill(PlayerDeathEvent ev) {
+    if (Games.ActiveGame is null) return;
+
+    var endGame = getWinningTeam(out var winningTeam);
+
+    if (!endGame) return;
+
+    Games.ActiveGame.EndGame(winningTeam);
+  }
+
+  private bool getWinningTeam(out IRole? winningTeam) {
+    var game = Games.ActiveGame;
+    winningTeam = null;
+    if (game is null) return false;
+
+    var traitorsAlive    = game.GetAlive(typeof(TraitorRole));
+    var nonTraitorsAlive = game.GetAlive().Count - traitorsAlive;
+    var detectivesAlive  = game.GetAlive(typeof(DetectiveRole));
+
+    switch (traitorsAlive) {
+      case 0 when nonTraitorsAlive == 0:
+        winningTeam = null;
+        return true;
+      case > 0 when nonTraitorsAlive == 0:
+        winningTeam = new TraitorRole(Provider);
+        return true;
+      case 0 when nonTraitorsAlive > 0:
+        winningTeam = nonTraitorsAlive == detectivesAlive ?
+          new DetectiveRole(Provider) :
+          new InnocentRole(Provider);
+        return true;
+      default:
+        winningTeam = null;
+        return false;
+    }
+  }
+}
