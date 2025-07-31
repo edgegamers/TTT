57338cd7fedccdb1df7f7c7c68029a3735c8099e
MSWS <imodmaker@gmail.com>
2025-07-31 14:56:46 -0700

    Implement FakePermissionManager

---
+++ TTT/Test/Fakes/FakePermissionManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Security;
+using TTT.API.Player;
+
+namespace TTT.Test.Fakes;
+
+public class FakePermissionManager : IPermissionManager {
+  public bool HasFlags(IPlayer player, params string[] flags) { return true; }
+
+  public bool InGroups(IPlayer player, params string[] groups) { return true; }
+}
