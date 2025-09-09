b0a8425aa3b2952ae848ed7949d69a7ab8e2e6dd
MSWS <14448584+MSWS@users.noreply.github.com>
2025-09-09 11:28:40 -0700

    Manually copy over old work of shop command

--- TTT/Shop/Commands/ShopCommand.cs
+++ TTT/Shop/Commands/ShopCommand.cs
@@ -1,10 +1,16 @@
+using CounterStrikeSharp.API.Modules.Utils;
 using Microsoft.Extensions.DependencyInjection;
 using TTT.API.Command;
 using TTT.API.Player;
+using TTT.Game;
+using TTT.Locale;
 
 namespace TTT.Shop.Commands;
 
 public class ShopCommand(IServiceProvider provider) : ICommand {
+  private readonly IMsgLocalizer locale = provider
+   .GetRequiredService<IMsgLocalizer>();
+
   private readonly Dictionary<string, ICommand> sub = new() {
     ["list"] = new ListCommand(provider)
   };
@@ -19,6 +25,20 @@ public class ShopCommand(IServiceProvider provider) : ICommand {
 
   public Task<CommandResult>
     Execute(IOnlinePlayer? executor, ICommandInfo info) {
+    HashSet<string> sent = [];
+    foreach (var (_, cmd) in sub) {
+      if (!sent.Add(cmd.Name)) continue;
+      var uses = cmd.Usage.Where(use => !string.IsNullOrWhiteSpace(use))
+       .ToList();
+      var useString = uses.Count > 0 ? "(" + string.Join(", ", uses) + ")" : "";
+      if (cmd.Description != null)
+        info.ReplySync(
+          $"{locale[GameMsgs.PREFIX]}{ChatColors.White}{cmd.Name} {ChatColors.Grey}- {ChatColors.BlueGrey}{cmd.Description}");
+      else
+        info.ReplySync(
+          $"{locale[GameMsgs.PREFIX]}{ChatColors.White}{cmd.Name} {ChatColors.Grey}{useString}");
+    }
+
     return Task.FromResult(CommandResult.ERROR);
   }
 }
