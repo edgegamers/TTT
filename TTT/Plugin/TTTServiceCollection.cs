c47c5db12ce0bb0b03093cb090313b021b9cc5f6
MSWS <14448584+MSWS@users.noreply.github.com>
2025-09-08 20:42:02 -0700

    feat: Introduce Karma module with DI and refactoring
    ```
    - Add `KarmaServiceCollection` for dependency injection setup, facilitating the registration of `IKarmaService` with scoped lifetime.
    - Update `TTTServiceCollection` to include new services for Karma management and necessary game services.
    - Refactor `KarmaStorage` to implement `IKarmaService`, simplifying interface responsibilities and removing previous interface implementations.
    - Configure `Plugin.csproj` with .NET 8.0, implicit usings, nullable reference types, and setup publish output handling.
    - Introduce `IKarmaService` interface to streamline access and interaction with karma functionalities.
    - Create `KarmaListener` class for managing karma adjustments during game events, implementing event handling and asynchronous tasks.
    ```

--- TTT/Plugin/TTTServiceCollection.cs
+++ TTT/Plugin/TTTServiceCollection.cs
@@ -3,6 +3,7 @@ using CounterStrikeSharp.API.Core;
 using Microsoft.Extensions.DependencyInjection;
 using TTT.CS2;
 using TTT.Game;
+using TTT.Karma;
 
 namespace TTT.Plugin;
 
@@ -10,6 +11,7 @@ public class TTTServiceCollection : IPluginServiceCollection<TTT> {
   public void ConfigureServices(IServiceCollection serviceCollection) {
     serviceCollection.AddScoped<IScheduler>(_ => Scheduler.Default);
 
+    serviceCollection.AddKarmaService();
     serviceCollection.AddGameServices();
     serviceCollection.AddCS2Services();
   }
