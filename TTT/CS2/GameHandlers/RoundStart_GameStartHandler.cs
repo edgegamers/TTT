5d5717317354b4198928c5ca0eb96a3722309c1e
MSWS <git@msws.xyz>
2025-09-27 19:07:28 -0700

    Check player count on round start before starting

--- TTT/CS2/GameHandlers/RoundStart_GameStartHandler.cs
+++ TTT/CS2/GameHandlers/RoundStart_GameStartHandler.cs
@@ -4,6 +4,7 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using TTT.API;
 using TTT.API.Game;
+using TTT.API.Player;
 using TTT.API.Storage;
 using TTT.Game;
 
@@ -14,6 +15,9 @@ public class RoundStart_GameStartHandler(IServiceProvider provider)
   private readonly TTTConfig config =
     provider.GetService<IStorage<TTTConfig>>()?.Load().GetAwaiter().GetResult()
     ?? new TTTConfig();
+
+  private readonly IPlayerFinder finder =
+    provider.GetRequiredService<IPlayerFinder>();
 
   private readonly IGameManager games =
     provider.GetRequiredService<IGameManager>();
@@ -27,6 +31,9 @@ public class RoundStart_GameStartHandler(IServiceProvider provider)
   public HookResult OnRoundStart(EventRoundStart _, GameEventInfo _1) {
     if (games.ActiveGame is { State: State.IN_PROGRESS or State.COUNTDOWN })
       return HookResult.Continue;
+
+    var count = finder.GetOnline().Count;
+    if (count < config.RoundCfg.MinimumPlayers) return HookResult.Continue;
 
     var game = games.CreateGame();
     game?.Start(config.RoundCfg.CountDownDuration);
