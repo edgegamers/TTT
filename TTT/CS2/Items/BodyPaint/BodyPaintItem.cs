4db898248a174592571922f41c290ef18e9e2505
MSWS <git@msws.xyz>
2025-10-02 23:44:00 -0700

    Apply default config to body paint

--- TTT/CS2/Items/BodyPaint/BodyPaintItem.cs
+++ TTT/CS2/Items/BodyPaint/BodyPaintItem.cs
@@ -3,6 +3,7 @@ using ShopAPI;
 using ShopAPI.Configs;
 using TTT.API.Extensions;
 using TTT.API.Player;
+using TTT.API.Storage;
 using TTT.Game.Roles;
 using TTT.Shop.Items.Traitor.BodyPaint;
 
@@ -17,12 +18,18 @@ public static class BodyPaintServicesCollection {
 
 public class BodyPaintItem(IServiceProvider provider)
   : RoleRestrictedItem<TraitorRole>(provider) {
+  private readonly BodyPaintConfig config = provider
+   .GetService<IStorage<BodyPaintConfig>>()
+  ?.Load()
+   .GetAwaiter()
+   .GetResult() ?? new BodyPaintConfig();
+
   public override string Name => Locale[BodyPaintMsgs.SHOP_ITEM_BODY_PAINT];
 
   public override string Description
     => Locale[BodyPaintMsgs.SHOP_ITEM_BODY_PAINT_DESC];
 
-  public override ShopItemConfig Config { get; }
+  public override ShopItemConfig Config => config;
 
   public override void OnPurchase(IOnlinePlayer player) { }
 }
