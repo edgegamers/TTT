4ceee412a7c1d3b3866d27d0d31cf2d7f520442b
MSWS <git@msws.xyz>
2025-10-02 21:18:39 -0700

    Fix role assignment logging

--- TTT/Game/Listeners/Loggers/PlayerActionsLogger.cs
+++ TTT/Game/Listeners/Loggers/PlayerActionsLogger.cs
@@ -9,23 +9,25 @@ namespace TTT.Game.Listeners.Loggers;
 public class PlayerActionsLogger(IServiceProvider provider)
   : BaseListener(provider) {
   // Needs to be higher so we detect the kill before the game ends
-  [EventHandler(Priority = Priority.HIGH)]
   [UsedImplicitly]
+  [EventHandler(Priority = Priority.HIGH)]
   public void OnPlayerKill(PlayerDeathEvent ev) {
     if (Games.ActiveGame is not { State: State.IN_PROGRESS }) return;
     Games.ActiveGame.Logger.LogAction(new DeathAction(Provider, ev));
   }
 
+
+  [UsedImplicitly]
   [EventHandler]
   public void OnPlayerDamage(PlayerDamagedEvent ev) {
     if (Games.ActiveGame is not { State: State.IN_PROGRESS }) return;
     Games.ActiveGame.Logger.LogAction(new DamagedAction(Provider, ev));
   }
 
+  [UsedImplicitly]
   [EventHandler]
   public void OnPlayerAssignedRole(PlayerRoleAssignEvent ev) {
-    if (Games.ActiveGame is not { State: State.IN_PROGRESS }) return;
-    Games.ActiveGame.Logger.LogAction(
+    Games.ActiveGame?.Logger.LogAction(
       new RoleAssignedAction(ev.Player, ev.Role));
   }
 }
