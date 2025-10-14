19e22e0f130f109393976278aa17e9b5f25c9de0
MSWS <git@msws.xyz>
2025-10-13 19:47:15 -0700

    Add karma grants per round

--- TTT/Karma/KarmaConfig.cs
+++ TTT/Karma/KarmaConfig.cs
@@ -47,6 +47,12 @@ public record KarmaConfig {
 
   public int MaxKarma(IPlayer? player) { return 100; }
 
+  /// <summary>
+  /// Amount of karma a player will gain at the end of each round.
+  /// </summary>
+  public int KarmaPerRound { get; init; } = 3;
+  public int KarmaPerRoundWin { get; init; } = 5;
+
   public int INNO_ON_TRAITOR { get; init; } = 2;
   public int TRAITOR_ON_DETECTIVE { get; init; } = 1;
   public int INNO_ON_INNO_VICTIM { get; init; } = -1;
