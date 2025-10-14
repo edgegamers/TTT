9ffd84fa660d783b20b33b6ee9ba19548b4df97a
MSWS <git@msws.xyz>
2025-10-13 20:16:21 -0700

    Buff poison shot amo (resolves #115)

--- TTT/ShopAPI/Configs/Traitor/PoisonShotsConfig.cs
+++ TTT/ShopAPI/Configs/Traitor/PoisonShotsConfig.cs
@@ -4,7 +4,7 @@ namespace ShopAPI.Configs.Traitor;
 
 public record PoisonShotsConfig : ShopItemConfig {
   public override int Price { get; init; } = 65;
-  public int TotalShots { get; init; } = 3;
+  public int TotalShots { get; init; } = 5;
   public Color PoisonColor { get; init; } = Color.FromArgb(128, Color.Purple);
   public PoisonConfig PoisonConfig { get; init; } = new();
 }
