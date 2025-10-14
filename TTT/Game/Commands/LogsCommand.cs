edb1229db5a48ab53d78ce466af077a0a1e9ae81
MSWS <git@msws.xyz>
2025-10-13 21:11:50 -0700

    feat: Enhance log viewing with messaging and visibility logic +semver:minor
    ```
    Enhance logging and visibility management in game commands and role handling
    
    - Update `LogsCommand.cs` to integrate messaging and localization with `IMessenger` and `IMsgLocalizer`, and conditionally adjust player visibility using `IIconManager`.
    - Add new localized message in `GameMsgs.cs` for player's log viewing activity, using player's name.
    - Overload `SetVisiblePlayers` in `IIconManager.cs` to increase flexibility by accepting an `IOnlinePlayer` object.
    - Implement `SetVisiblePlayers` in `RoleIconsHandler.cs` to improve player visibility and role management, and integrate rules for specific roles like `DetectiveRole` and `TraitorRole`.
    - Modify `en.yml` to include a log message for when a player views logs alive.
    ```

--- TTT/Game/Commands/LogsCommand.cs
+++ TTT/Game/Commands/LogsCommand.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using TTT.API.Command;
 using TTT.API.Game;
+using TTT.API.Messages;
 using TTT.API.Player;
+using TTT.Locale;
 
 namespace TTT.Game.Commands;
 
 public class LogsCommand(IServiceProvider provider) : ICommand {
   private readonly IGameManager games =
     provider.GetRequiredService<IGameManager>();
+
+  private readonly IMessenger messenger =
+    provider.GetRequiredService<IMessenger>();
+
+  private readonly IMsgLocalizer localizer =
+    provider.GetRequiredService<IMsgLocalizer>();
+  
+  private readonly IIconManager? icons =
+    provider.GetService<IIconManager>();
 
   public void Dispose() { }
   public string[] RequiredFlags => ["@ttt/admin"];
@@ -22,6 +33,13 @@ public class LogsCommand(IServiceProvider provider) : ICommand {
     }) {
       info.ReplySync("No active game to show logs for.");
       return Task.FromResult(CommandResult.ERROR);
+    }
+
+    if (executor is { IsAlive: true })
+      messenger.MessageAll(localizer[GameMsgs.LOGS_VIEWED_ALIVE(executor)]);
+    else if (icons != null && executor != null) {
+      if(int.TryParse(executor.Id, out var slot))
+        icons.SetVisiblePlayers(slot, ulong.MaxValue);
     }
 
     games.ActiveGame.Logger.PrintLogs(executor);
