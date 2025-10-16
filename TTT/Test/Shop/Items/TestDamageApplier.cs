acb9d971e0502d02634b6bad3e2f29d6811aa1d7
MSWS <git@msws.xyz>
2025-10-16 13:12:49 -0700

    Fix failing tests

---
+++ TTT/Test/Shop/Items/TestDamageApplier.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using TTT.API.Events;
+using TTT.API.Player;
+using TTT.Game.Events.Player;
+using TTT.Game.Listeners;
+
+namespace TTT.Test.Shop.Items;
+
+public class TestDamageApplier(IServiceProvider provider)
+  : BaseListener(provider) {
+  [UsedImplicitly]
+  [EventHandler(Priority = Priority.MONITOR)]
+  public void OnDamage(PlayerDamagedEvent ev) {
+    if (ev.Player is not IOnlinePlayer online) return;
+
+    online.Health = Math.Clamp(ev.HpLeft, 0, online.MaxHealth);
+  }
+}
