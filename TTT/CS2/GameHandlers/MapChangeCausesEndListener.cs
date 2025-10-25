f83b6efac49e6f8c2e249b034b0076391d57af9f
MSWS <git@msws.xyz>
2025-10-25 01:59:16 -0700

    Handle map change interrupting rounds

---
+++ TTT/CS2/GameHandlers/MapChangeCausesEndListener.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using TTT.API;
+using TTT.API.Game;
+
+namespace TTT.CS2.GameHandlers;
+
+public class MapChangeCausesEndListener(IServiceProvider provider)
+  : IPluginModule {
+  private readonly IGameManager games =
+    provider.GetRequiredService<IGameManager>();
+
+  public void Dispose() { }
+  public void Start() { }
+
+  [UsedImplicitly]
+  [GameEventHandler]
+  public HookResult OnMapChange(EventMapTransition ev, GameEventInfo _) {
+    games.ActiveGame?.EndGame(new EndReason("Map Change"));
+    return HookResult.Continue;
+  }
+}
