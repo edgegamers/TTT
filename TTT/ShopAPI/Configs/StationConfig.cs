a596a66d9625cd137802884aba28520acbee36dd
MSWS <git@msws.xyz>
2025-10-25 02:25:11 -0700

    Reduce M4 cost, reduce station health

--- TTT/ShopAPI/Configs/StationConfig.cs
+++ TTT/ShopAPI/Configs/StationConfig.cs
@@ -5,7 +5,7 @@ namespace ShopAPI.Configs;
 public abstract record StationConfig : ShopItemConfig {
   public virtual int HealthIncrements { get; init; } = 5;
   public virtual int TotalHealthGiven { get; init; } = 0;
-  public virtual int StationHealth { get; init; } = 1000;
+  public virtual int StationHealth { get; init; } = 200;
   public virtual float MaxRange { get; init; } = 256;
 
   public virtual TimeSpan HealthInterval { get; init; } =
