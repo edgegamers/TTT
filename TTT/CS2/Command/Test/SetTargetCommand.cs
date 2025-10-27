dda9bea7e63e38b8a77425e2e94813a7af7015f8
MSWS <git@msws.xyz>
2025-10-26 20:16:32 -0700

    Add settarget debug

---
+++ TTT/CS2/Command/Test/SetTargetCommand.cs
@@ -0,0 +1,26 @@
+﻿using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.DependencyInjection;
+using TTT.API.Command;
+using TTT.API.Player;
+
+namespace TTT.CS2.Command.Test;
+
+public class SetTargetCommand(IServiceProvider provider) : ICommand {
+  private readonly IPlayerConverter<CCSPlayerController> converter =
+    provider.GetRequiredService<IPlayerConverter<CCSPlayerController>>();
+
+  public void Dispose() { }
+  public void Start() { }
+
+  public Task<CommandResult>
+    Execute(IOnlinePlayer? executor, ICommandInfo info) {
+    if (executor == null) return Task.FromResult(CommandResult.PLAYER_ONLY);
+    var gamePlayer = converter.GetPlayer(executor);
+    if (gamePlayer == null) return Task.FromResult(CommandResult.ERROR);
+
+    info.ReplySync("Target: " + gamePlayer.Target);
+    gamePlayer.Target = "traitor";
+    info.ReplySync("New Target: " + gamePlayer.Target);
+    return Task.FromResult(CommandResult.SUCCESS);
+  }
+}
---
+++ TTT/CS2/Roles/CS2TraitorRole.cs
@@ -0,0 +1,21 @@
+﻿using CounterStrikeSharp.API.Core;
+using Microsoft.Extensions.DependencyInjection;
+using TTT.API.Player;
+using TTT.Game.Roles;
+
+namespace TTT.CS2.Roles;
+
+public class CS2TraitorRole(IServiceProvider provider) : TraitorRole(provider) {
+  private readonly IPlayerConverter<CCSPlayerController> converter =
+    provider.GetRequiredService<IPlayerConverter<CCSPlayerController>>();
+
+  public override void OnAssign(IOnlinePlayer player) {
+    base.OnAssign(player);
+
+    var gamePlayer = converter.GetPlayer(player);
+    if (gamePlayer == null) return;
+
+    gamePlayer.AcceptInput("SetTargetName", null, null, "traitor");
+    if (gamePlayer.Pawn.Value != null) gamePlayer.Pawn.Value.Target = "traitor";
+  }
+}
