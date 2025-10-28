e487ed9ffab97afbe84bed49c7aa73dae0c18470
MSWS <git@msws.xyz>
2025-10-28 10:58:49 -0700

    Revert "Add debug"
    This reverts commit 70c416bbe68ca8d86f9c44a83c025abad8826032.

--- TTT/CS2/Command/Test/SetTargetCommand.cs
+++ TTT/CS2/Command/Test/SetTargetCommand.cs
@@ -29,7 +29,6 @@ public class SetTargetCommand(IServiceProvider provider) : ICommand {
       var csPlayer = converter.GetPlayer(executor);
       if (csPlayer == null) return;
       EntityNameHelper.SetEntityName(csPlayer, EntityNameHelper.Role.Traitor);
-      info.ReplySync($"Set entity name for player {executor.Name}");
     });
     return Task.FromResult(CommandResult.SUCCESS);
   }
