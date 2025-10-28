131489c5212799ea509eafef4fab48eef9d3f055
MSWS <git@msws.xyz>
2025-10-28 13:21:40 -0700

    Use prefix

--- TTT/API/Game/IAction.cs
+++ TTT/API/Game/IAction.cs
@@ -21,7 +21,7 @@ public interface IAction {
       $" [{OtherRole.Name.First(char.IsAsciiLetter)}]" :
       "";
     return Other is not null ?
-      $"{Player}{pRole} {Verb} {Other}{oRole} {Details}" :
-      $"{Player}{pRole} {Verb} {Details}";
+      $"{Prefix}{Player}{pRole} {Verb} {Other}{oRole} {Details}" :
+      $"{Prefix}{Player}{pRole} {Verb} {Details}";
   }
 }
