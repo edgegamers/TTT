8fa55a100287984a06df9eeb7215a6aa970fdc02
MSWS <git@msws.xyz>
2025-11-01 16:41:12 -0700

    Reduce cost of compasses

--- TTT/ShopAPI/Configs/Traitor/CompassConfig.cs
+++ TTT/ShopAPI/Configs/Traitor/CompassConfig.cs
@@ -1,7 +1,7 @@
 namespace ShopAPI.Configs.Traitor;
 
 public record CompassConfig : ShopItemConfig {
-  public override int Price { get; init; } = 70;
+  public override int Price { get; init; } = 60;
   public float MaxRange { get; init; } = 10000;
   public float CompassFOV { get; init; } = 120;
   public int CompassLength { get; init; } = 64;
