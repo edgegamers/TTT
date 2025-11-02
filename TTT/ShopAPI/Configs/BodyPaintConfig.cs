b3ed2449807b4140b24035d918c3994f4eb61564
MSWS <git@msws.xyz>
2025-11-01 17:00:41 -0700

    Reduce cost of body paint

--- TTT/ShopAPI/Configs/BodyPaintConfig.cs
+++ TTT/ShopAPI/Configs/BodyPaintConfig.cs
@@ -3,7 +3,7 @@ using System.Drawing;
 namespace ShopAPI.Configs;
 
 public record BodyPaintConfig : ShopItemConfig {
-  public override int Price { get; init; } = 40;
+  public override int Price { get; init; } = 30;
   public int MaxUses { get; init; } = 4;
   public Color ColorToApply { get; init; } = Color.GreenYellow;
 }
