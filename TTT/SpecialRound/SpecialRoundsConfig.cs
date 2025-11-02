c470c431ffaa6499cbc5f9ad630a7d764b1db43a
MSWS <git@msws.xyz>
2025-11-02 01:28:16 -0700

    update: Reduce min requirement for special round 8 -> 5

--- TTT/SpecialRound/SpecialRoundsConfig.cs
+++ TTT/SpecialRound/SpecialRoundsConfig.cs
@@ -2,7 +2,7 @@
 
 public record SpecialRoundsConfig {
   public int MinRoundsBetweenSpecial { get; init; } = 3;
-  public int MinPlayersForSpecial { get; init; } = 8;
+  public int MinPlayersForSpecial { get; init; } = 5;
   public int MinRoundsAfterMapChange { get; init; } = 2;
   public float SpecialRoundChance { get; init; } = 0.2f;
 }
