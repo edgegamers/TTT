8ba831c915dd4e92b75d2006cceb8b7791e505a5
MSWS <git@msws.xyz>
2025-11-02 20:40:05 -0800

    fix: Add check after roles assigned

---
+++ TTT/SpecialRound/Events/AbstractSpecialRoundEvent.cs
@@ -0,0 +1,9 @@
+﻿using SpecialRoundAPI;
+using TTT.API.Events;
+
+namespace SpecialRound.Events;
+
+public abstract class AbstractSpecialRoundEvent(AbstractSpecialRound round)
+  : Event {
+  public AbstractSpecialRound Round => round;
+}
---
+++ TTT/SpecialRound/Events/SpecialRoundStartEvent.cs
@@ -0,0 +1,9 @@
+﻿using SpecialRoundAPI;
+using TTT.API.Events;
+
+namespace SpecialRound.Events;
+
+public class SpecialRoundStartEvent(AbstractSpecialRound round)
+  : AbstractSpecialRoundEvent(round), ICancelableEvent {
+  public bool IsCanceled { get; set; }
+}
