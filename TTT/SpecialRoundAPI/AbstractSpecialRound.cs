e5fcc2ac42db7be50c52acedba5bece58ac441d2
MSWS <git@msws.xyz>
2025-11-05 04:38:51 -0800

    Reapply "fix: Re-add moved files"
    This reverts commit 9145e16b9029cdea14cf01bf09dd44370fb742ec.

---
+++ TTT/SpecialRoundAPI/AbstractSpecialRound.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using SpecialRoundAPI.Configs;
+using TTT.API.Events;
+using TTT.Game.Events.Game;
+using TTT.Game.Listeners;
+using TTT.Locale;
+
+namespace SpecialRoundAPI;
+
+public abstract class AbstractSpecialRound(IServiceProvider provider)
+  : BaseListener(provider) {
+  protected readonly ISpecialRoundTracker Tracker =
+    provider.GetRequiredService<ISpecialRoundTracker>();
+
+  public abstract string Name { get; }
+  public abstract IMsg Description { get; }
+  public abstract SpecialRoundConfig Config { get; }
+
+  public abstract void ApplyRoundEffects();
+
+  [UsedImplicitly]
+  [EventHandler]
+  public abstract void OnGameState(GameStateUpdateEvent ev);
+}
---
+++ TTT/SpecialRoundAPI/ISpecialRoundStarter.cs
@@ -0,0 +1,13 @@
+﻿namespace SpecialRoundAPI;
+
+public interface ISpecialRoundStarter {
+  /// <summary>
+  ///   Attempts to start the given special round.
+  ///   Will bypass most checks, but may still return null if starting the round
+  ///   is not possible.
+  /// </summary>
+  /// <param name="round"></param>
+  /// <returns></returns>
+  public AbstractSpecialRound?
+    TryStartSpecialRound(AbstractSpecialRound? round);
+}
