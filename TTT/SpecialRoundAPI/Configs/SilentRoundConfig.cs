b6d01447312eebcc8153e6cc06278afe5fa7eb81
MSWS <git@msws.xyz>
2025-12-01 19:23:04 -0800

    update: Decrease silent round probability

--- TTT/SpecialRoundAPI/Configs/SilentRoundConfig.cs
+++ TTT/SpecialRoundAPI/Configs/SilentRoundConfig.cs
@@ -1,5 +1,5 @@
 ﻿namespace SpecialRoundAPI.Configs;
 
 public record SilentRoundConfig : SpecialRoundConfig {
-  public override float Weight { get; init; } = 0.5f;
+  public override float Weight { get; init; } = 0.2f;
 }
