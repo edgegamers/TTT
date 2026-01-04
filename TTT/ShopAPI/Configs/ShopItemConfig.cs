a46cdb53073c5491f5c01690dffc2808709cb9c5
ShookEagle <shookeagle@gmail.com>
2026-01-04 14:12:05 -0600

    Add LimitMode and Limit properties to ShopItemConfig

--- TTT/ShopAPI/Configs/ShopItemConfig.cs
+++ TTT/ShopAPI/Configs/ShopItemConfig.cs
@@ -2,4 +2,7 @@ namespace ShopAPI.Configs;
 
 public abstract record ShopItemConfig {
   public abstract int Price { get; init; }
+  
+  public virtual ItemLimitMode LimitMode { get; init; } = ItemLimitMode.OFF;
+  public virtual int Limit { get; init; } = 0;
 }
