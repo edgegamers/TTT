146f1a31789417473f1a0284121ab38704f2f146
ShookEagle <shookeagle@gmail.com>
2026-01-04 14:11:49 -0600

    Add ItemLimitMode enum to define item purchase limitations

---
+++ TTT/ShopAPI/ItemLimitMode.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopAPI;
+
+public enum ItemLimitMode {
+  /// <summary>
+  ///   No Limit. Players May purchase as many items as they want.
+  /// </summary>
+  OFF,
+  
+  /// <summary>
+  ///   Limits the item per player. Up to the Set Limit.
+  /// </summary>
+  PER_PLAYER,
+  
+  /// <summary>
+  ///   Limits the item for the entire team. Up to the Set Limit.
+  /// </summary>
+  PER_TEAM
+}
