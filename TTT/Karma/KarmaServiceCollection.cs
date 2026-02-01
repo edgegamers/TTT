1ed944a4e109485037b30f84e0ce3e2cecdb86d3
pdtttt <ptripodi0@saddleback.edu>
2026-02-01 07:19:55 -0800

    Testing: Hotfix for karma

--- TTT/Karma/KarmaServiceCollection.cs
+++ TTT/Karma/KarmaServiceCollection.cs
@@ -5,7 +5,8 @@ namespace TTT.Karma;
 
 public static class KarmaServiceCollection {
   public static void AddKarmaService(this IServiceCollection collection) {
-    collection.AddModBehavior<IKarmaService, KarmaStorage>();
+    // collection.AddModBehavior<IKarmaService, KarmaStorage>();
+    collection.AddModBehavior<IKarmaService, DictionaryKarmaStorage>();
     collection.AddModBehavior<KarmaListener>();
     collection.AddModBehavior<KarmaCommand>();
   }
