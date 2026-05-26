cffc35e3f5a84620689c1306e60f1fd67b59ccd9
ShookEagle <shookeagle@gmail.com>
2026-05-25 21:48:46 -0500

    Add `DesignerNameMatchType` to `TryGetHitEntityByDesignerName` for flexible designer name matching

--- TTT/CS2/Extensions/TraceResultExtensions.cs
+++ TTT/CS2/Extensions/TraceResultExtensions.cs
@@ -4,29 +4,43 @@ using RayTraceAPI;
 namespace TTT.CS2.Extensions;
 
 public static class TraceResultExtensions {
+  public enum DesignerNameMatchType {
+    Equals, StartsWith, EndsWith, Contains
+  }
+
   public static bool TryGetHitEntityByDesignerName<T>(this TraceResult trace,
-    string designerNameContains, out T? entity) where T : CEntityInstance
-  {
+    string designerName, out T? entity,
+    DesignerNameMatchType matchType = DesignerNameMatchType.Contains)
+    where T : CEntityInstance {
     entity = null;
 
     if (!trace.DidHit || trace.HitEntity == 0) return false;
 
-    var baseEntity = new CEntityInstance(trace.HitEntity);
-    if (!baseEntity.IsValid) return false;
-
-    if (string.IsNullOrWhiteSpace(baseEntity.DesignerName)) return false;
-
-    if (!baseEntity.DesignerName.Contains(designerNameContains, StringComparison.OrdinalIgnoreCase))
+    if (Activator.CreateInstance(typeof(T), trace.HitEntity) is not T {
+      IsValid: true
+    } typedEntity)
       return false;
 
-    try
-    {
-      var typedEntity = (T?)Activator.CreateInstance(typeof(T), trace.HitEntity);
-      if (typedEntity == null || !typedEntity.IsValid) return false;
+    entity = matchType switch {
+      DesignerNameMatchType.Equals => typedEntity.DesignerName.Equals(
+        designerName, StringComparison.OrdinalIgnoreCase) ?
+        typedEntity :
+        null,
+      DesignerNameMatchType.StartsWith => typedEntity.DesignerName.StartsWith(
+        designerName, StringComparison.OrdinalIgnoreCase) ?
+        typedEntity :
+        null,
+      DesignerNameMatchType.EndsWith => typedEntity.DesignerName.EndsWith(
+        designerName, StringComparison.OrdinalIgnoreCase) ?
+        typedEntity :
+        null,
+      DesignerNameMatchType.Contains => typedEntity.DesignerName.Contains(
+        designerName, StringComparison.OrdinalIgnoreCase) ?
+        typedEntity :
+        null,
+      _ => null
+    };
 
-      entity = typedEntity;
-      return true;
-    }
-    catch { return false; }
+    return entity != null;
   }
 }
