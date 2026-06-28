0d5d712edf86d9923424d8c0feb89a24e148a906
Gabriel Hirakawa <gabriel@gkh.dev>
2026-06-28 11:45:07 -0700

    feat(rdm): scaffold RDM module + RdmConfig

---
+++ TTT/RDM/RdmServiceCollection.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TTT.RDM;
+
+public static class RdmServiceCollection {
+  public static void AddRdmService(this IServiceCollection collection) {
+    // Services are registered by later tasks.
+  }
+}
